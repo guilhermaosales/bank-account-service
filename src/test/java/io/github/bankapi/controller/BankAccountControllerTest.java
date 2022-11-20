c534262f283ddf2374c0d22be125c22d6e761cc1
Guilherme Sales <guilhermewrsales@gmail.com>
2022-11-20 20:42:09 -0300

    test: finish controller

--- src/test/java/io/github/bankapi/controller/BankAccountControllerTest.java
+++ src/test/java/io/github/bankapi/controller/BankAccountControllerTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.netbeans.lib.cvsclient.commandLine.command.update;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import static io.github.bankapi.mock.BankAccountMocks.bankAccountFormMock;
 import static io.github.bankapi.mock.BankAccountMocks.bankAccountResponseMock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -47,36 +47,78 @@ class BankAccountControllerTest {
     @MockBean
     private BankAccountServiceImpl service;
 
-    public static UUID ID = BankAccountMocks.bankId;
+    private static final UUID ID = BankAccountMocks.bankId;
+    private static final String BANK_ACCOUNT_URI = "/bank-account";
 
     @Test
-    void shouldCreateABankAccountSuccessfully() throws Exception {
+    void shouldCreateBankAccountSuccessfully() throws Exception {
 
-        when(service.createBankAccount(any(BankAccountForm.class))).thenReturn(bankAccountResponseMock());
+        ArgumentCaptor<BankAccountForm> bankAccountCaptor = ArgumentCaptor.forClass(BankAccountForm.class);
 
-        mockMvc.perform(post("/bank-account")
+        when(service.createBankAccount(bankAccountCaptor.capture())).thenReturn(bankAccountResponseMock());
+
+        mockMvc.perform(post(BANK_ACCOUNT_URI)
                 .contentType("application/json")
                 .content(objectMapper.writeValueAsString(bankAccountFormMock())))
                 .andDo(print())
                 .andExpect(status().isCreated());
 
         var actual = controller.createBankAccount(bankAccountFormMock());
-        
-        ArgumentCaptor<BankAccountForm> bankAccountCaptor = ArgumentCaptor.forClass(BankAccountForm.class);
+
         verify(service, atLeastOnce()).createBankAccount(bankAccountCaptor.capture());
 
         assertEquals(bankAccountFormMock(), bankAccountCaptor.getValue());
-        assertEquals(actual, new ResponseEntity<>(bankAccountResponseMock(), HttpStatus.CREATED));
+        assertEquals(new ResponseEntity<>(bankAccountResponseMock(), HttpStatus.CREATED), actual);
     }
 
     @Test
-    void shouldDeleteAccountSuccessfully() throws Exception {
+    void shouldDeleteBankAccountSuccessfully() throws Exception {
 
         doNothing().when(service).deleteBankAccount(ID);
 
+        mockMvc.perform(delete(BANK_ACCOUNT_URI.concat("/{id}"), ID)
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isAccepted());
+
         var actual = controller.deleteBankAccount(ID);
 
-        assertEquals(actual, new ResponseEntity<>(HttpStatus.ACCEPTED));
+        assertEquals(new ResponseEntity<>(HttpStatus.ACCEPTED), actual);
 
+    }
+
+    @Test
+    void shouldReturnBankAccountSuccessfully() throws Exception {
+
+        when(service.getOneBankAccount(ID)).thenReturn(bankAccountResponseMock());
+
+        mockMvc.perform(get(BANK_ACCOUNT_URI.concat("/{id}"), ID)
+                .contentType("application/json"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        var actual = controller.getOneBankAccount(ID);
+        
+        verify(service, atLeastOnce()).getOneBankAccount(ID);
+
+        assertEquals(new ResponseEntity<>(bankAccountResponseMock(), HttpStatus.OK), actual);
+    }
+
+    @Test
+    void shouldUpdateBankAccountSuccessfully() throws Exception {
+
+        when(service.updateBankAccount(ID, bankAccountFormMock())).thenReturn(bankAccountResponseMock());
+
+        mockMvc.perform(put(BANK_ACCOUNT_URI.concat("/{id}"), ID)
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(bankAccountFormMock())))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        var actual = controller.updateBankAccount(ID, bankAccountFormMock());
+
+        verify(service, atLeastOnce()).updateBankAccount(ID, bankAccountFormMock());
+
+        assertEquals(new ResponseEntity<>(bankAccountResponseMock(), HttpStatus.OK), actual);
     }
 }
